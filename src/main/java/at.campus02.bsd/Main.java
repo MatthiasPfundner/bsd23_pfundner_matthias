60c390aabe714d4b9e94667b9f52bf96dfd6658f
MatthiasPfundner <s53017@edu.campus02.at>
2024-05-20 18:41:22 +0200

    Added two images. Changed the Log4J versions in the pom.xml. Generated a new logging object and added two logging entries to the main

--- src/main/java/at.campus02.bsd/Main.java
+++ src/main/java/at.campus02.bsd/Main.java
@@ -7,10 +7,15 @@
 
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * main method to execute calculations and print the results.
  */
 public class Main {
+    private static final Logger logger = LogManager.getLogger();
+
     public static void main(String[] args) {
         double numb1 = 10.0;
         double numb2 = 5.0;
@@ -20,5 +25,8 @@ public class Main {
         System.out.println(Calculator.minus(numb1, numb2));
         System.out.println(Calculator.divide(numb1, numb2));
         System.out.println(Calculator.multiply(numb1, numb2));
+
+        logger.info("This is a info-log entry.");
+        logger.error("This is a error-log entry.");
     }
 }
